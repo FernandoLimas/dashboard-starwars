d2b994b0cb5981e39f3bb665c6f3ab03a9db3bee
FernandoLimas <nando_filipe@hotmail.com>
2022-01-11 12:31:15 -0300

    starwars

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,7 +1,29 @@
-const URL = "https://swapi.dev/api/people/1/";
+// DOM
+const personCount = document.getElementById("persons");
+const planetsCount = document.getElementById("planets");
+const vehiclesCount = document.getElementById("vehicles");
+const shipsCount = document.getElementById("ships");
 
-fetch(URL)
-  .then(response => response.json())
-  .then(data => console.log(data));
-  
-console.log('ok');
+function getSwapi(param) {
+  return axios.get(`https://swapi.dev/api/${param}`);
+}
+
+function fillCount(){
+Promise.all([
+  getSwapi("people/"),
+  getSwapi("planets/"),
+  getSwapi("vehicles/"),
+  getSwapi("starships/"),
+])
+  .then(function (results) {
+    personCount.innerHTML = results[0].data.count;
+    planetsCount.innerHTML = results[1].data.count;
+    vehiclesCount.innerHTML = results[2].data.count;
+    shipsCount.innerHTML = results[3].data.count;
+    
+    console.log(results);
+  });
+
+}
+
+fillCount()
